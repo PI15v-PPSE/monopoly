274dbe7523836c1c46da57fd4eb70f1061edb23e
Gumbeat <leo2598@yandex.ua>
2018-03-30 01:22:22 +0300

    merged unmergable
    Signed-off-by: Gumbeat <leo2598@yandex.ua>

--- scripts/classicedition.js
+++ scripts/classicedition.js
@@ -27,45 +27,11 @@ function Square(name, pricetext, color, price, groupNumber, baserent, rent1, ren
     } else {
         this.houseprice = 0;
     }
-class Square {
-    constructor (name, pricetext, color, price, groupNumber, baserent, rent1, rent2, rent3, rent4, rent5) {
-        this.name = name
-        this.pricetext = pricetext
-        this.color = color
-        this.owner = 0
-        this.mortgage = false
-        this.house = 0
-        this.hotel = 0
-        this.groupNumber = groupNumber || 0
-        this.price = (price || 0)
-        this.baserent = (baserent || 0)
-        this.rent1 = (rent1 || 0)
-        this.rent2 = (rent2 || 0)
-        this.rent3 = (rent3 || 0)
-        this.rent4 = (rent4 || 0)
-        this.rent5 = (rent5 || 0)
-        this.landcount = 0
-
-        if (groupNumber === 3 || groupNumber === 4) {
-            this.houseprice = 50
-        } else if (groupNumber === 5 || groupNumber === 6) {
-            this.houseprice = 100
-        } else if (groupNumber === 7 || groupNumber === 8) {
-            this.houseprice = 150
-        } else if (groupNumber === 9 || groupNumber === 10) {
-            this.houseprice = 200
-        } else {
-            this.houseprice = 0
-        }
-    }
 }
 
-function Card(text, action) {
-    this.text = text;
-    this.action = action;
 class Card {
     constructor (text, action) {
-        this.text = text
+        this.text = text;
         this.action = action
     }
 }
@@ -80,28 +46,14 @@ function corrections() {
     document.getElementById("enlarge35token").innerHTML += '<img src="../images/train_icon.png" height="60" width="65" alt="" style="position: relative; top: -20px;" />';
     document.getElementById("enlarge12token").innerHTML += '<img src="../images/electric_icon.png" height="60" width="48" alt="" style="position: relative; top: -20px;" />';
     document.getElementById("enlarge28token").innerHTML += '<img src="../images/water_icon.png" height="60" width="78" alt="" style="position: relative; top: -20px;" />';
-function corrections () {
-    document.getElementById("cell1name").textContent = "Mediter-ranean Avenue"
-
-    // Add images to enlarges.
-    document.getElementById("enlarge5token").innerHTML += '<img src="../images/train_icon.png" height="60" width="65" alt="" style="position: relative; bottom: 20px;" />'
-    document.getElementById("enlarge15token").innerHTML += '<img src="../images/train_icon.png" height="60" width="65" alt="" style="position: relative; top: -20px;" />'
-    document.getElementById("enlarge25token").innerHTML += '<img src="../images/train_icon.png" height="60" width="65" alt="" style="position: relative; top: -20px;" />'
-    document.getElementById("enlarge35token").innerHTML += '<img src="../images/train_icon.png" height="60" width="65" alt="" style="position: relative; top: -20px;" />'
-    document.getElementById("enlarge12token").innerHTML += '<img src="../images/electric_icon.png" height="60" width="48" alt="" style="position: relative; top: -20px;" />'
-    document.getElementById("enlarge28token").innerHTML += '<img src="../images/water_icon.png" height="60" width="78" alt="" style="position: relative; top: -20px;" />'
 }
 
 function utiltext() {
     return '&nbsp;&nbsp;&nbsp;&nbsp;If one "Utility" is owned rent is 4 times amount shown on dice.<br /><br />&nbsp;&nbsp;&nbsp;&nbsp;If both "Utilitys" are owned rent is 10 times amount shown on dice.';
-function utiltext () {
-    return '&nbsp;&nbsp;&nbsp;&nbsp;If one "Utility" is owned rent is 4 times amount shown on dice.<br /><br />&nbsp;&nbsp;&nbsp;&nbsp;If both "Utilitys" are owned rent is 10 times amount shown on dice.'
 }
 
 function transtext() {
     return '<div style="font-size: 14px; line-height: 1.5;">Rent<span style="float: right;">$25.</span><br />If 2 Railroads are owned<span style="float: right;">50.</span><br />If 3 &nbsp; &nbsp; " &nbsp; &nbsp; " &nbsp; &nbsp; "<span style="float: right;">100.</span><br />If 4 &nbsp; &nbsp; " &nbsp; &nbsp; " &nbsp; &nbsp; "<span style="float: right;">200.</span></div>';
-function transtext () {
-    return '<div style="font-size: 14px; line-height: 1.5;">Rent<span style="float: right;">$25.</span><br />If 2 Railroads are owned<span style="float: right;">50.</span><br />If 3 &nbsp; &nbsp; " &nbsp; &nbsp; " &nbsp; &nbsp; "<span style="float: right;">100.</span><br />If 4 &nbsp; &nbsp; " &nbsp; &nbsp; " &nbsp; &nbsp; "<span style="float: right;">200.</span></div>'
 }
 
 function luxurytax() {
@@ -109,69 +61,60 @@ function luxurytax() {
     player[turn].pay(100, 0);
 
     $("#landed").show().text("Вы попали на Luxury Tax. Заплатите $100.");
-function luxurytax () {
-    addAlert(player[turn].name + " заплатите $100 за попадание на Luxury Tax.")
-    player[turn].pay(100, 0)
-
-    $("#landed").show().text("Вы попали на Luxury Tax. Заплатите $100.")
+}
 }
 
-function citytax() {
+function citytax () {
     addAlert(player[turn].name + " заплатите $100 за попадание на City Tax.");
     player[turn].pay(200, 0);
-function citytax () {
-    addAlert(player[turn].name + " заплатите $100 за попадание на City Tax.")
-    player[turn].pay(200, 0)
-
     $("#landed").show().text("Вы попали на City Tax. Заплатите $200.");
-    $("#landed").show().text("Вы попали на City Tax. Заплатите $200.")
 }
 
 let square = [];
 
-square[0] = new Square("СТАРТ", "ПОЛУЧИТЕ $200 ПРИ ПРОХОДЕ ЧЕРЕЗ ЭТУ КЛЕТКУ.", "#FFFFFF")
-square[1] = new Square("Проспект Богдан Хмельницкого", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250)
-square[2] = new Square("Community Chest", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF")
-square[3] = new Square("ОПЛОТ", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450)
-square[4] = new Square("Первое республиканское такси", "Заплатите $200", "#FFFFFF")
-square[5] = new Square("Донецкий ЖД вокзал", "$200", "#FFFFFF", 200, 1)
-square[6] = new Square("Площадь Ленина", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550)
-square[7] = new Square("Коммендантский час", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF")
-square[8] = new Square("Бургер Бар", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550)
-square[9] = new Square("Connecticut Avenue", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600)
-square[10] = new Square("Just Visiting", "", "#FFFFFF")
-square[11] = new Square("St. Charles Place", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750)
-square[12] = new Square("Electric Company", "$150", "#FFFFFF", 150, 2)
-square[13] = new Square("States Avenue", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750)
-square[14] = new Square("Virginia Avenue", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900)
-square[15] = new Square("Pennsylvania Railroad", "$200", "#FFFFFF", 200, 1)
-square[16] = new Square("St. James Place", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950)
-square[17] = new Square("Community Chest", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF")
-square[18] = new Square("Tennessee Avenue", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950)
-square[19] = new Square("New York Avenue", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000)
-square[20] = new Square("Free Parking", "", "#FFFFFF")
-square[21] = new Square("Kentucky Avenue", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050)
-square[22] = new Square("Chance", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF")
-square[23] = new Square("Indiana Avenue", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050)
-square[24] = new Square("Illinois Avenue", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100)
-square[25] = new Square("B&O Railroad", "$200", "#FFFFFF", 200, 1)
-square[26] = new Square("Atlantic Avenue", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150)
-square[27] = new Square("Ventnor Avenue", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150)
-square[28] = new Square("Water Works", "$150", "#FFFFFF", 150, 2)
-square[29] = new Square("Marvin Gardens", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200)
-square[30] = new Square("Go to Jail", "Попадаете прямиком в тюрьму. Не проходите через клетку GO. Не получаете $200", "#FFFFFF")
-square[31] = new Square("Pacific Avenue", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275)
-square[32] = new Square("North Carolina Avenue", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275)
-square[33] = new Square("Community Chest", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF")
-square[34] = new Square("Pennsylvania Avenue", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400)
-square[35] = new Square("Short Line", "$200", "#FFFFFF", 200, 1)
-square[36] = new Square("Chance", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF")
-square[37] = new Square("Park Place", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500)
-square[38] = new Square("LUXURY TAX", "Заплатите $100", "#FFFFFF")
-square[39] = new Square("Boardwalk", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000)
+square[0] = new Square("СТАРТ", "ПОЛУЧИТЕ $200 ПРИ ПРОХОДЕ ЧЕРЕЗ ЭТУ КЛЕТКУ.", "#FFFFFF");
+square[1] = new Square("Проспект Богдан Хмельницкого", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250);
+square[2] = new Square("Community Chest", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF");
+square[3] = new Square("ОПЛОТ", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450);
+square[4] = new Square("Первое республиканское такси", "Заплатите $200", "#FFFFFF");
+square[5] = new Square("Донецкий ЖД вокзал", "$200", "#FFFFFF", 200, 1);
+square[6] = new Square("Площадь Ленина", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
+square[7] = new Square("Коммендантский час", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF");
+square[8] = new Square("Бургер Бар", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
+square[9] = new Square("Connecticut Avenue", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600);
+square[10] = new Square("Just Visiting", "", "#FFFFFF");
+square[11] = new Square("St. Charles Place", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
+square[12] = new Square("Electric Company", "$150", "#FFFFFF", 150, 2);
+square[13] = new Square("States Avenue", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
+square[14] = new Square("Virginia Avenue", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900);
+square[15] = new Square("Pennsylvania Railroad", "$200", "#FFFFFF", 200, 1);
+square[16] = new Square("St. James Place", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
+square[17] = new Square("Community Chest", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF");
+square[18] = new Square("Tennessee Avenue", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
+square[19] = new Square("New York Avenue", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000);
+square[20] = new Square("Free Parking", "", "#FFFFFF");
+square[21] = new Square("Kentucky Avenue", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[22] = new Square("Chance", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF");
+square[23] = new Square("Indiana Avenue", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[24] = new Square("Illinois Avenue", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100);
+square[25] = new Square("B&O Railroad", "$200", "#FFFFFF", 200, 1);
+square[26] = new Square("Atlantic Avenue", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[27] = new Square("Ventnor Avenue", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[28] = new Square("Water Works", "$150", "#FFFFFF", 150, 2);
+square[29] = new Square("Marvin Gardens", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200);
+square[30] = new Square("Go to Jail", "Попадаете прямиком в тюрьму. Не проходите через клетку GO. Не получаете $200", "#FFFFFF");
+square[31] = new Square("Pacific Avenue", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275);
+square[32] = new Square("North Carolina Avenue", "$300", "#008000", 300, 9, 26, 130, 390, 900, 110, 1275);
+square[33] = new Square("Community Chest", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF");
+square[34] = new Square("Pennsylvania Avenue", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400);
+square[35] = new Square("Short Line", "$200", "#FFFFFF", 200, 1);
+square[36] = new Square("Chance", "СЛЕДУЙТЕ ИНСТРУКЦИЯМ НА ВЕРХНЕЙ КАРТОЧКЕ", "#FFFFFF");
+square[37] = new Square("Park Place", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500);
+square[38] = new Square("LUXURY TAX", "Заплатите $100", "#FFFFFF");
+square[39] = new Square("Boardwalk", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000);
 
-let communityChestCards = []
-let chanceCards = []
+let communityChestCards = [];
+let chanceCards = [];
 
 communityChestCards[0] = new Card("Выйти из тюрьмы, Свобода. Эту карточку можно сохранить до момента надобности или продажи .", function (p) {
     p.communityChestJailCard = true;
